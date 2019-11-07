0e0479dd8616bf915a530fb54fa2d301a9026f91
jtucci31 <50505759+jtucci31@users.noreply.github.com>
2019-11-07 14:08:06 -0600

    ESPN cleaning

--- Data/1-ESPN-health-data/ESPN_JSON.py
+++ Data/1-ESPN-health-data/ESPN_JSON.py
@@ -12,31 +12,80 @@ import pathlib
 data = json.loads(pathlib.Path('stadiums_espn.json').read_text())
 
 
-print(type(data))
-
-for record in data:
-    print(record)
+# print(type(data))
+#
+# for record in data:
+#     print(record)
 
 
 # print(len(data['items']))
 #
 # print(data['items'])
-
-names = [d['name'] for d in data['items']]
-city = [d['city']for d in data['items']]
-state = [d['state_abbrev'] for d in data['items']]
-violations = [d['inspections'] for d in data['items']]
-violations_specific = [d['inspections'] for d in data['items']]
-sports = [d['sports'] for d in data['items']]
-critical_score = [d['critical'] for d in data['items']]
-teams = [d['teams'] for d in data['items']]
-
-print(teams)
+#
+# name = [d['name'] for d in data['items']]
+# city = [d['city']for d in data['items']]
+# state = [d['state_abbrev'] for d in data['items']]
+# violations = [d['inspections'] for d in data['items']]
+# violations_specific = [d['inspections'] for d in data['items']]
+# sports = [d['sports'] for d in data['items']]
+# critical_score = [d['critical'] for d in data['items']]
+# teams = [d['teams'] for d in data['items']]
 
 
 for record in data['items']:
     if 'MLB' in record['sports']:
-        print(record['name'] + "/", record['city'], record['inspections'])
+        print(record['name'] + " /", record['city'], record['inspections'], record['teams'])
+
+def clean_team(record):
+    if len(record) > 1:
+        result = record[1]
+    else:
+        result = record[0]
+    return result
+
+# for record in data['items']:
+#     mlb_teams = clean_team(record['teams'])
+#     print(mlb_teams)
+
+all_stadiums = []
+
+for record in data['items']:
+    if 'MLB' in record['sports']:
+        stadium_name = record['name']
+        mlb_teams = clean_team(record['teams'])
+        city = record['city']
+        state_abbrev = record['state_abbrev']
+        state = record['state']
+        critical_score = record['critical']
+        capacity = record['critical']
+        venue_score = record['venue']
+        area_score = record['area']
+        inspections = record['inspections']
+        violations = record['violations']
+
+        row = []
+        row.append(mlb_teams)
+        row.append(stadium_name)
+        row.append(capacity)
+        row.append(city)
+        row.append(state_abbrev)
+        row.append(state)
+        row.append(critical_score)
+        row.append(venue_score)
+        row.append(area_score)
+        row.append(inspections)
+        row.append(violations)
+        all_stadiums.append(row)
+
+
+csv_file = open('ESPN_stadium_violations.csv', 'w', newline = '')
+csvout = csv.writer(csv_file)
+csvout.writerow(['MLB Team', 'Stadium Name', 'Seating Capacity', 'City', 'State Abbreviation', 'State', 'Critical Score in percentage', 'Venue Score', 'Area Score', 'Inspections', 'Violations'])
+
+csvout.writerows(all_stadiums)
+csv_file.close()
+
+
 
 
 
---
+++ Data/4-Voice-of-Fan-Report-2017/4_Voice_of_fan_report.py
@@ -0,0 +1,6 @@
+
+infile = open("Voice_of_Fan_Report_2017.CSV", "rt")
+
+allrows = infile.read()
+
+print(allrows)
